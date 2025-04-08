6ca62682fab637c1479362659b756b1a6a5eb91e
H4d3ssss <francisco.ralffieheart.doctolero@gmail.com>
2025-04-09 01:35:57 +0800

    Full calendar implemented still needs improvement and adjustment that will align with the fmlawfirm system

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Navbar, Calendar, Sidebar, Footer } from './components'
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Calendar />
+      <Sidebar />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/src/components/index.js
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Footer from "./Footer";
+import Calendar from "./Calendar";
+
+export {
+    Navbar,
+    Sidebar,
+    Footer,
+    Calendar,
+}
