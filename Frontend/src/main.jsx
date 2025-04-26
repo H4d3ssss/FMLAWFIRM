2f9d91f86f05118cc17b394d2b385bc74b012182
Jv Bialen <162081127+meliodaff@users.noreply.github.com>
2025-04-26 21:06:59 +0800

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import AppTest from "./AppTest.jsx";
-import AppTest1 from "./AppTest1.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
